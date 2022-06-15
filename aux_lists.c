ad82e57441546fcd817fdc321c2fa00a48fd7707
LeoMaina <leonardhophni@gmail.com>
2022-06-15 13:10:10 -0700

    Initial commit

--- aux_lists.c
+++ aux_lists.c
@@ -1,80 +1,111 @@
 #include "main.h"
 
 /**
- * aux_help_env - Help information for the builtin env
- * Return: no return
+ * add_sep_node_end - adds a separator found at the end
+ * of a sep_list.
+ * @head: head of the linked list.
+ * @sep: separator found (; | &).
+ * Return: address of the head.
  */
-void aux_help_env(void)
+sep_list *add_sep_node_end(sep_list **head, char sep)
 {
-	char *help = "env: env [option] [name=value] [command [args]]\n\t";
+	sep_list *new, *temp;
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Print the enviroment of the shell.\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+	new = malloc(sizeof(sep_list));
+	if (new == NULL)
+		return (NULL);
 
-}
-/**
- * aux_help_setenv - Help information for the builtin setenv
- * Return: no return
- */
-void aux_help_setenv(void)
-{
+	new->separator = sep;
+	new->next = NULL;
+	temp = *head;
 
-	char *help = "setenv: setenv (const char *name, const char *value,";
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "int replace)\n\t";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Add a new definition to the environment\n";
-	write(STDOUT_FILENO, help, _strlen(help));
-}
-/**
- * aux_help_unsetenv - Help information for the builtin unsetenv
- * Return: no return
- */
-void aux_help_unsetenv(void)
-{
-	char *help = "unsetenv: unsetenv (const char *name)\n\t";
-
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Remove an entry completely from the environment\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+	return (*head);
 }
 
-
 /**
- * aux_help_general - Entry point for help information for the help builtin
- * Return: no return
+ * free_sep_list - frees a sep_list
+ * @head: head of the linked list.
+ * Return: no return.
  */
-void aux_help_general(void)
+void free_sep_list(sep_list **head)
 {
-	char *help = "^-^ bash, version 1.0(1)-release\n";
+	sep_list *temp;
+	sep_list *curr;
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "These commands are defined internally.Type 'help' to see the list";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Type 'help name' to find out more about the function 'name'.\n\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = " alias: alias [name=['string']]\n cd: cd [-L|[-P [-e]] [-@]] ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "[dir]\nexit: exit [n]\n  env: env [option] [name=value] [command ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "[args]]\n  setenv: setenv [variable] [value]\n  unsetenv: ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "unsetenv [variable]\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
-/**
- * aux_help_exit - Help information fot the builint exit
- * Return: no return
- */
-void aux_help_exit(void)
-{
-	char *help = "exit: exit [n]\n Exit shell.\n";
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Exits the shell with a status of N. If N is ommited, the exit";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "statusis that of the last command executed\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+/**
+ * add_line_node_end - adds a command line at the end
+ * of a line_list.
+ * @head: head of the linked list.
+ * @line: command line.
+ * Return: address of the head.
+ */
+line_list *add_line_node_end(line_list **head, char *line)
+{
+	line_list *new, *temp;
+
+	new = malloc(sizeof(line_list));
+	if (new == NULL)
+		return (NULL);
+
+	new->line = line;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
+ * free_line_list - frees a line_list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+void free_line_list(line_list **head)
+{
+	line_list *temp;
+	line_list *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
